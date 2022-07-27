8ce615b2e3213c3605a8c90549a27837a9d87924
null <975959057@qq.com>
2022-07-27 18:12:48 +0800

    update

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,10 +1,14 @@
 import request from '@/http/request'
 
 export async function adminAuth(params: Param.AdminAuth) {
-  return await request.get('/plugin/admin/auth', params)
+  return await request.get<Result.Token>('/plugin/admin/auth', params)
 }
 
-export async function adminCommand(params: Param.AdminCommand) {
+export async function adminCreateAccount(params: Param.Account) {
+  return await request.get<Result.Token>('/plugin/admin/createAccount', params)
+}
+
+export async function adminCommand(params: Param.Command) {
   return await request.get('/plugin/admin/command', params)
 }
 
@@ -12,10 +16,14 @@ export async function mailVerifyCode(params: Param.MailVerifyCode) {
   return await request.get('/plugin/mail/verifyCode', params)
 }
 
-export async function playerAuth(params: Param.PlayerAuth) {
-  return await request.get('/plugin/player/auth', params)
+export async function playerAuthByVerifyCode(params: Param.VerifyCode) {
+  return await request.get<Result.Token>('/plugin/player/authByVerifyCode', params)
 }
 
-export async function playerCommand(params: Param.PlayerCommand) {
+export async function playerAuthByPassword(params: Param.Account) {
+  return await request.get<Result.Token>('/plugin/player/authByPassword', params)
+}
+
+export async function playerCommand(params: Param.Command) {
   return await request.get('/plugin/player/command', params)
 }
