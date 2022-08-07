1b610c53e0c1c2f8bccd8f6d5f5ce39cb6f000d9
null <975959057@qq.com>
2022-08-07 21:31:46 +0800

    v1.1.0

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="flex-center flex-col gap-5 text-5">
     <span>{{ t('t1') }}null</span>
-    <span>{{ t('t2') }}1.0.2</span>
+    <span>{{ t('t2') }}1.1.0</span>
+    <span>{{ t('t3') }}1.1.0</span>
     <div>
-      <span>{{ t('t3') }}</span>
+      <span>{{ t('t4') }}</span>
       <n-a href="https://github.com/jianxingxuejian/grasscutter-tools" target="_blank">
         https://github.com/jianxingxuejian/grasscutter-tools
       </n-a>
     </div>
     <div>
-      <span>{{ t('t4') }}</span>
+      <span>{{ t('t5') }}</span>
       <n-a href="https://github.com/jianxingxuejian/grasscutter-plugin" target="_blank">
         https://github.com/jianxingxuejian/grasscutter-plugin
       </n-a>
@@ -26,17 +27,19 @@
 <i18n locale="zh-CN" lang="json">
 {
   "t1": "作者：",
-  "t2": "版本：",
-  "t3": "项目地址：",
-  "t4": "服务端所需插件："
+  "t2": "客户端版本：",
+  "t3": "插件版本：",
+  "t4": "项目地址：",
+  "t5": "服务端所需插件："
 }
 </i18n>
 
 <i18n locale="en-US" lang="json">
 {
   "t1": "Author：",
-  "t2": "Version：",
-  "t3": "Project address：",
-  "t4": "Server plugins："
+  "t2": "Client version：",
+  "t3": "Plugin version：",
+  "t4": "Project address：",
+  "t5": "Server plugins："
 }
 </i18n>
