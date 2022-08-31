014a02859665e38f705a36d7b8c7d50128c2af72
null <975959057@qq.com>
2022-08-31 16:02:30 +0800

    update

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,5 +1,3 @@
-use app::unwrap_else;
-use app::unwrap_or;
 use std::collections::HashMap;
 use std::ffi::OsStr;
 use std::fs::File;
@@ -14,25 +12,30 @@ pub async fn get_mod_list(path: String) -> HashMap<String, String> {
     let ini = Some(OsStr::new("ini"));
     let mut map = HashMap::new();
     for entry in WalkDir::new(path) {
-        let entry = entry.unwrap();
-        if entry.path().extension() != ini {
-            continue;
-        }
-        let mod_dir = unwrap_or!(entry.path().parent(), continue);
-        let parent = unwrap_or!(mod_dir.parent(), continue);
-
-        let modinfo = parent.join("modinfo.json");
-        print!("{}", modinfo.display());
-        if modinfo.exists() {
-            let mut file = File::open(&modinfo).unwrap();
-            let mut string = String::new();
-            file.read_to_string(&mut string).unwrap();
-            map.insert(parent.display().to_string(), string);
-        } else {
-            let string = "{\"id\":0,\"name\":\"\",\"images\":[],\"submitter\":{\"name\":\"\",\"url\":\"\"},\"nsfw\":false,\"likes\":0,\"views\":0,\"type\":\"Mod\"}";
-            let mut file = File::create(&modinfo).unwrap();
-            unwrap_else!(file.write(string.as_bytes()), continue);
-            map.insert(parent.display().to_string(), string.to_string());
+        let result = || -> Option<(String, String)> {
+            let entry = entry.ok()?;
+            if entry.path().extension() != ini {
+                return None;
+            }
+            let parent = entry.path().parent()?.parent()?;
+            let modinfo = parent.join("modinfo.json");
+            if modinfo.exists() {
+                let mut file = File::open(&modinfo).unwrap();
+                let mut info = String::new();
+                file.read_to_string(&mut info).unwrap();
+                return Some((parent.display().to_string(), info));
+            } else {
+                let info = "{\"id\":0,\"name\":\"\",\"images\":[],\"submitter\":{\"name\":\"\",\"url\":\"\"},\"nsfw\":false,\"likes\":0,\"views\":0,\"type\":\"Mod\"}";
+                let mut file = File::create(&modinfo).unwrap();
+                file.write(info.as_bytes()).ok();
+                return Some((parent.display().to_string(), info.to_string()));
+            }
+        };
+        match result() {
+            Some((k, v)) => {
+                map.insert(k, v);
+            }
+            None => continue,
         }
     }
     map
