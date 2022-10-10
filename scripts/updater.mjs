e8e907dc5b746391ae251fbf73e07572a69621c8
null <975959057@qq.com>
2022-10-11 00:38:10 +0800

    test

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -10,6 +10,30 @@ export async function updater() {
     throw new Error('GITHUB_TOKEN is required')
   }
 
+  const notes = {}
+
+  i18nList.forEach(async locale => {
+    const logPath = path.join(process.cwd(), `CHANGELOG.${locale}.md`)
+    if (!(await fs.pathExists(logPath))) {
+      throw new Error(`could not found CHANGELOG.${locale}.md`)
+    }
+
+    const read = fs.readFileSync(logPath, 'utf-8').split('\n')
+    const changelog = []
+    let count = 0
+    for (const line of read) {
+      if (count > 1) break
+      if (line.startsWith('##') || line.startsWith('-')) {
+        changelog.push(line)
+      }
+      if (/^## [v[\d.]+/.test(line)) {
+        count++
+      }
+    }
+
+    notes[locale] = changelog.join('/n')
+  })
+
   const options = { owner: context.repo.owner, repo: context.repo.repo }
   const github = getOctokit(process.env.GITHUB_TOKEN)
   const { tag_name, published_at } = (await github.rest.repos.getLatestRelease({ ...options })).data
@@ -26,7 +50,7 @@ export async function updater() {
   const updateData = {
     version: tag_name,
     pub_date: published_at,
-    notes: '',
+    notes,
     platforms: {
       win64: {
         url: downloadUrl_win,
@@ -60,38 +84,16 @@ export async function updater() {
   }
 
   const { data: updateRelease } = await github.rest.repos.getReleaseByTag({ ...options, tag: 'updater' })
+  const asset = updateRelease.assets.find(item => item.name.includes('update'))
+  if (asset) {
+    await github.rest.repos.deleteReleaseAsset({ ...options, asset_id: asset.id })
+  }
 
-  i18nList.forEach(async locale => {
-    const logPath = path.join(process.cwd(), `CHANGELOG.${locale}.md`)
-    if (!(await fs.pathExists(logPath))) {
-      throw new Error(`could not found CHANGELOG.${locale}.md`)
-    }
-
-    const asset = updateRelease.assets.find(item => item.name.includes(locale))
-
-    const read = fs.readFileSync(logPath, 'utf-8').split('\n')
-    const changelog = []
-    let count = 0
-    for (const line of read) {
-      if (count > 1) break
-      if (line.startsWith('##') || line.startsWith('-')) {
-        changelog.push(line)
-      }
-      if (/^## [v[\d.]+/.test(line)) {
-        count++
-      }
-    }
-    updateData.notes = changelog.join('/n')
-    if (asset) {
-      await github.rest.repos.deleteReleaseAsset({ ...options, asset_id: asset.id })
-    }
-
-    await github.rest.repos.uploadReleaseAsset({
-      ...options,
-      release_id: updateRelease.id,
-      name: `update_${locale}.json`,
-      data: JSON.stringify(updateData)
-    })
+  await github.rest.repos.uploadReleaseAsset({
+    ...options,
+    release_id: updateRelease.id,
+    name: `update.json`,
+    data: JSON.stringify(updateData)
   })
 }
 
