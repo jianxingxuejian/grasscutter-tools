0681b73804521da1b41791d6597798c4d84f06dc
null <975959057@qq.com>
2022-10-22 17:12:16 +0800

    chore: export components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import MyButton from './MyButton.vue'
+import MyDivider from './MyDivider.vue'
+import MyInputGroup from './MyInputGroup.vue'
+import MySelect from './MySelect.vue'
+import MySwitch from './MySwitch.vue'
+import NaiveProvider from './NaiveProvider.vue'
+
+export { MyButton, MyDivider, MyInputGroup, MySelect, MySwitch, NaiveProvider }
