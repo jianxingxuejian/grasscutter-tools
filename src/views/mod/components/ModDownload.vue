d8f13236c64e81966f8d5b8bcba89f0ad4d16be0
null <975959057@qq.com>
2022-10-25 23:46:36 +0800

    feat: test online mods list, #9

--- src/views/mod/components/ModDownload.vue
+++ src/views/mod/components/ModDownload.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <img src="https://images.gamebanana.com/img/ss/mods/220-90_635467e7a1ec6.jpg" />
-  </div>
+  <n-grid cols="s:5 m:6 l:7 xl:8 2xl:9" responsive="screen" class="px-4">
+    <n-gi v-for="(item, index) in modDataList" :key="index" class="h-auto p-1 flex-col">
+      <n-image lazy preview-disabled object-fit="contain" :src="item.images[0]" class="rd-1 cursor-pointer" @click="open(item.url)" />
+    </n-gi>
+  </n-grid>
 </template>
 
 <script setup lang="ts">
   import { fetch } from '@tauri-apps/api/http'
+  import { open } from '@tauri-apps/api/shell'
 
   interface ModDataBody {
     _aMetadata: {
@@ -13,22 +16,29 @@
     }
     _aRecords: {
       _sName: string
+      _sProfileUrl: string
       _nViewCount?: number
       _nLikeCount?: number
-      _nPostCount?: number
       _aPreviewMedia: {
         _aImages: { _sBaseUrl: string; _sFile220: string }[]
       }
+      _aSubmitter: { _sName: string; _sProfileUrl: string }
     }[]
   }
 
   interface ModData {
     name: string
-    auth: string
+    url: string
+    author: string
+    authorUrl: string
     images: string[]
+    viewCount: number
+    likeCount: number
   }
 
-  onActivated(async () => {
+  const modDataList = ref<ModData[]>([])
+
+  async function getModDataBody() {
     const response = await fetch<ModDataBody>(
       'https://gamebanana.com/apiv10/Mod/Index?_nPage=1&_nPerpage=20&_aFilters%5BGeneric_Game%5D=8552',
       {
@@ -38,9 +48,19 @@
     )
     const { status, data } = response
     if (status === 200) {
-      console.log(data)
-      const a = data._aRecords[0]._aPreviewMedia._aImages[0]._sBaseUrl
-      console.log(a)
+      modDataList.value = data._aRecords.map(({ _sName, _sProfileUrl, _aSubmitter, _nViewCount, _nLikeCount, _aPreviewMedia }) => ({
+        name: _sName,
+        url: _sProfileUrl,
+        author: _aSubmitter._sName,
+        authorUrl: _aSubmitter._sProfileUrl,
+        viewCount: _nViewCount ?? 0,
+        likeCount: _nLikeCount ?? 0,
+        images: _aPreviewMedia._aImages.map(({ _sBaseUrl, _sFile220 }) => _sBaseUrl + '/' + _sFile220)
+      }))
     }
+  }
+
+  onActivated(async () => {
+    getModDataBody()
   })
 </script>
