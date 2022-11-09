b2357c3715d946e55a4018e249a4e4a3521d368a
null <975959057@qq.com>
2022-11-09 23:45:33 +0800

    fix: github pages build fix

---
+++ build/config/utils.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
+export function sanitizeFileName(name: string): string {
+  const match = DRIVE_LETTER_REGEX.exec(name)
+  const driveLetter = match ? match[0] : ''
+
+  // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+  // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+  return driveLetter + name.substring(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+}
