da96abb966ece2b063a0ea845322d7051067c615
null <975959057@qq.com>
2022-11-19 15:13:58 +0800

    fix: wrong type

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -39,7 +39,7 @@
     /** 按钮文本 */
     text?: string
     /** 异步点击事件 */
-    onClickAsync?: () => Promise<void | ApiResult<null>> | void | undefined
+    onClickAsync?: () => Promise<void | ApiResult<null>>
     /** 点击事件 */
     onClick?: () => void
   }>()
