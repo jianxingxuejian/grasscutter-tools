326dc413be60193a198e30e5d78ee820f86d9ea6
jianxingxuejian <jianxingxuejian@users.noreply.github.com>
2022-11-22 15:41:42 +0000

    deploy: 190ea11ce0001fe0ba1b5efa4c1bf33493f71ff4

---
+++ assets/a.da30ca50.js
@@ -0,0 +1,8 @@
+import{a as c,d,e as u,u as r,g as h,i as m,h as f,bh as p}from"./index.2a5b893b.js";const v=c("a",`
+ cursor: pointer;
+ transition:
+ color .3s var(--n-bezier),
+ text-decoration-color .3s var(--n-bezier);
+ text-decoration-color: var(--n-text-color);
+ color: var(--n-text-color);
+`),C=Object.assign({},r.props),x=d({name:"A",props:C,setup(e){const{mergedClsPrefixRef:o,inlineThemeDisabled:n}=u(e),a=r("Typography","-a",v,p,e,o),t=h(()=>{const{common:{cubicBezierEaseInOut:i},self:{aTextColor:l}}=a.value;return{"--n-text-color":l,"--n-bezier":i}}),s=n?m("a",void 0,t,e):void 0;return{mergedClsPrefix:o,cssVars:n?void 0:t,themeClass:s==null?void 0:s.themeClass,onRender:s==null?void 0:s.onRender}},render(){var e;return(e=this.onRender)===null||e===void 0||e.call(this),f("a",{class:[`${this.mergedClsPrefix}-a`,this.themeClass],style:this.cssVars},this.$slots)}});export{x as _};
