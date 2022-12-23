cc26a52ac9e187e0c3156d2237b2ea9ae4943d53
null <975959057@qq.com>
2022-12-24 02:44:25 +0800

    feat: other system support

--- src-tauri/src/cmd/system.rs
+++ src-tauri/src/cmd/system.rs
@@ -2,8 +2,8 @@ use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
-use std::process::{Command, Output};
+use std::path::{Path, PathBuf};
+use std::process::{Child, Command, Output};
 use tauri::api::path::data_dir;
 
 #[cfg(target_os = "windows")]
@@ -43,16 +43,7 @@ pub fn run_jar(path: String) -> Result<(), Box<dyn Error>> {
 
 #[cfg(target_os = "windows")]
 pub fn execute_luac(path: String, contents: String) -> Result<String, Box<dyn Error>> {
-    let parent = data_dir()
-        .ok_or("Cannot open data folder")?
-        .join("grasscutter-tools")
-        .join("lua");
-    let lua_path = parent.join("dev.lua");
-    if !parent.exists() {
-        fs::create_dir_all(&parent)?;
-    }
-    let mut file = File::create(&lua_path)?;
-    file.write_all(contents.as_bytes())?;
+    let lua_path = write_lua(contents)?;
     let index = path.rfind("/").ok_or("path error")?;
     let (path, file) = path.split_at(index + 1);
     let cmd_str = format!(
@@ -62,15 +53,15 @@ pub fn execute_luac(path: String, contents: String) -> Result<String, Box<dyn Er
         lua_path.display()
     );
     let child = Command::new("cmd").raw_arg(cmd_str).spawn()?;
-    let output = child.wait_with_output()?;
-    if !output.status.success() {
-        let output_str = String::from_utf8(output.stderr)?;
-        return Err(output_str.into());
-    }
-    let out_path = Path::new(&path).join("dev.luac");
-    let bytes = fs::read(out_path)?;
-    let result = base64::encode(&bytes);
-    Ok(result)
+    return_luac(child, path)
+}
+
+#[cfg(not(target_os = "windows"))]
+pub fn execute_luac(path: String, contents: String) -> Result<String, Box<dyn Error>> {
+    let lua_path = write_lua(contents)?;
+    let cmd_str = format!("-c {} -o dev.luac {}", path, lua_path.display());
+    let child = Command::new("cmd").raw_arg(cmd_str).spawn()?;
+    return_luac(child, &path)
 }
 
 fn return_output(output: Output) -> Result<String, Box<dyn Error>> {
@@ -81,4 +72,30 @@ fn return_output(output: Output) -> Result<String, Box<dyn Error>> {
         let output_str = String::from_utf8(output.stderr)?;
         Err(output_str.into())
     }
+}
+
+fn write_lua(contents: String) -> Result<PathBuf, Box<dyn Error>> {
+    let parent = data_dir()
+        .ok_or("Cannot open data folder")?
+        .join("grasscutter-tools")
+        .join("lua");
+    let lua_path = parent.join("dev.lua");
+    if !parent.exists() {
+        fs::create_dir_all(&parent)?;
+    }
+    let mut file = File::create(&lua_path)?;
+    file.write_all(contents.as_bytes())?;
+    Ok(lua_path)
+}
+
+fn return_luac(child: Child, path: &str) -> Result<String, Box<dyn Error>> {
+    let output = child.wait_with_output()?;
+    if !output.status.success() {
+        let output_str = String::from_utf8(output.stderr)?;
+        return Err(output_str.into());
+    }
+    let out_path = Path::new(path).join("dev.luac");
+    let bytes = fs::read(out_path)?;
+    let result = base64::encode(&bytes);
+    Ok(result)
 }
