0e5436296530153dcd0aa69b65c675e60ff491fa
null <975959057@qq.com>
2023-01-05 14:19:34 +0800

    fix: missing

--- src-tauri/src/cmd/system.rs
+++ src-tauri/src/cmd/system.rs
@@ -24,8 +24,8 @@ pub fn run_program(path: String, args: Option<String>) -> Result<String, Box<dyn
 }
 
 #[cfg(not(target_os = "windows"))]
-pub fn run_program(path: String) -> Result<String, Box<dyn Error>> {
-    let cmd_str = format!("-c open {}", path);
+pub fn run_program1(path: String, args: Option<String>) -> Result<String, Box<dyn Error>> {
+    let cmd_str = format!("-c open {} {}", path, args.map_or("".to_string(), |s| s));
     let output = Command::new("bash").arg(cmd_str).output()?;
     return_output(output)
 }
